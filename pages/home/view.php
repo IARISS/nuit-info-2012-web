b6694d5bd944131044c777320074ccebde7a44e6
Olivarès Georges <olivares.georges@gmail.com>
2012-12-06 19:28:41 +0100

    Page Home/view

---
+++ pages/home/view.php
@@ -0,0 +1,11 @@
+<form id="home-search" class="form-search" method="GET" action="/search">
+  <div>
+    <input type="text" name="search" value="" class="input-xxlarg span6" placeholder="Entrez votre recherche (ex: Musée, Alsace, Etoffes, ...)" />
+  </div>
+</form>
+
+<ul id="home-cloudtags">
+  <?php foreach( array('tag', 'tag2', 'tag3') as $tag ): ?>
+  <li><a href="?tags=<php echo $tag; ?>"><?php echo $tag; ?></a></li>
+  <?php endforeach; ?>
+</ul>
