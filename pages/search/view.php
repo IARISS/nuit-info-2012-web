d229bb9dd0ee0942f773f617ecc9e913e7c0d898
Olivarès Georges <olivares.georges@gmail.com>
2012-12-06 19:47:08 +0100

    search/view

--- pages/search/view.php
+++ pages/search/view.php
@@ -1,8 +1,41 @@
-<div class="row">
-  <div class="span2">
-    a
+<form id="search-search" class="form-search" method="GET" action="/search">
+  <div>
+    <input type="text" name="search" value="" class="input-xxlarg span5" placeholder="Entrez votre recherche (ex: Musée, Alsace, Etoffes, ...)" />
   </div>
-  <div class="span2">
-    b
+</form>
+
+<div class="row">
+  <div id="search-column" class="span2 dblock">
+    <div>
+      <h1>Related Tags</h1>
+      <ul>
+        <?php foreach( array('tag', 'tag2', 'tag3') as $tag ): ?>
+        <li><a href="?tags=<php echo $tag; ?>"><?php echo $tag; ?></a></li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+  </div>
+  <div class="span10 dblock" id="search-items">
+    <div>
+      <article>
+        <div class="row-fluid">
+          <div class="span2">
+            <figure class="img-polaroid">
+            </figure>
+          </div>
+          <div class="span9">
+            <h1>Title</h1>
+            <ul class="tags">
+              <?php foreach( array('tag', 'tag2', 'tag3') as $tag ): ?>
+              <li><a href="?tags=<php echo $tag; ?>"><?php echo $tag; ?></a></li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+          <div class="span1">
+            &gt;
+          </div>
+        </div>
+      </article>
+    </div>
   </div>
 </div>
