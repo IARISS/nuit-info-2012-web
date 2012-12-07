d017d916c9d52d8365ccb6de6d096d15b9a55e3c
Karl Woditsch <k.woditsch@gmail.com>
2012-12-07 05:38:25 +0100

    404 lulz

--- pages/errors/404.php
+++ pages/errors/404.php
@@ -1,6 +1,9 @@
 <div id="error">
   <h1>Error 404 !</h1>
   <div id="here"><i class="icon-screenshot icon-white"></i> You are ... somewhere ... here</div>
+  <div id="orhere"><i class="icon-screenshot icon-white"></i> Or ... here</div>
+  <div id="maybehere"><i class="icon-screenshot icon-white"></i> Wait ... maybe here</div>
+  <div id="what"><i class="icon-screenshot icon-white"></i> What is this ... you here ?</div>
 </div>
 
 <style>
@@ -12,4 +15,7 @@
   #error { color: white; }
 
   #here { position: absolute; bottom: 50%; right: 40%; font-size: 2em; }
+  #orhere { position: absolute; bottom: 20%; right: 30%; font-size: 2em; }
+  #maybehere { position: absolute; bottom: 70%; right: 20%; font-size: 2em; }
+  #what { position: absolute; bottom: 60%; right: 70%; font-size: 2em; }
 </style>
