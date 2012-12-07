138f3f0b3800198bc2b91e1d6e578ab4b2e2ac02
Karl Woditsch <k.woditsch@gmail.com>
2012-12-07 05:26:19 +0100

    h1 h2 imprima

--- lib/tag/tag.php
+++ lib/tag/tag.php
@@ -106,7 +106,7 @@ class Tag {
     $toLearn = array();
     foreach($words as $word){
       if(substr($word, 0, 1) == '#'){
-        $toLearn[] = str_replace('#', '', $word));
+        $toLearn[] = str_replace('#', '', $word);
       }
     }
     $know = Tag::getTagsNameIn($toLearn);
