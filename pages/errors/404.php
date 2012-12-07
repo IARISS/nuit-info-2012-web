45e25d4db7590bfbc4fb535196757a2c0658b63f
Olivarès Georges <olivares.georges@gmail.com>
2012-12-07 03:29:04 +0100

    Error 404

--- pages/errors/404.php
+++ pages/errors/404.php
@@ -1,1 +1,15 @@
-<h1>Error 404 !</h1>
+<div id="error">
+  <h1>Error 404 !</h1>
+  <div id="here"><i class="icon-screenshot icon-white"></i> You are ... somewhere ... here</div>
+</div>
+
+<style>
+  body {
+    background-image: url('http://fond-d-ecran-gratuit.org/photos/espace.jpg');
+    background-position: center center;
+  }
+
+  #error { color: white; }
+
+  #here { position: absolute; bottom: 50%; right: 40%; font-size: 2em; }
+</style>
