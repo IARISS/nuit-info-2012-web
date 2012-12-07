f17cfe25578e2953b5adf36c2d63a157b2f0e331
Olivarès Georges <olivares.georges@gmail.com>
2012-12-07 05:24:23 +0100

    Style caribou

--- pages/home/view.php
+++ pages/home/view.php
@@ -1,9 +1,11 @@
 <form id="home-search" class="form-search" method="GET" action="./search">
   <div class="input-append">
-    <input type="text" name="search" value="" class="input-xxlarg span6" placeholder="Entrez votre recherche (ex: Musée, Alsace, Etoffes, ...)" />
-    <button class="btn" type="submit">Rechercher !</button>
+    <input id="search-input" type="text" name="search" value="" class="input-xxlarg span6" placeholder="Entrez votre recherche (ex: Musée, Alsace, Etoffes, ...)" />
+    <button id="search-go" class="btn" type="submit">Rechercher !</button>
   </div>
 </form>
+
+<div id="caribou"></div>
 
 <ul id="home-cloudtags">
   <?php foreach( $entitiesTopTags as $tag ): ?>
@@ -14,19 +16,28 @@
 <?php
 
 $_javascripts[] = '<script>
+(function () {
   if ( window.addEventListener ) {
-    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-    window.addEventListener("keydown", function(e) {
-      kkeys.push( e.keyCode );
-      if ( kkeys.toString().indexOf( konami ) >= 0 ) {
-        window.location.href = "?theme=caribou";
-      }
-    }, true);
-  }
+    var state = 0,
+    JeSuisUnCaribou = [74,69,32,83,85,73,83,32,85,78,32,67,65,82,73,66,79,85];
 
-  $(document).ready(function() {
-    $(\'input[name="search"]\').focus();
-  });
+    window.addEventListener("keydown", function(e) {
+     if ( e.keyCode == JeSuisUnCaribou[state] )
+      state++;
+    else
+      state = 0;
+
+    if ( state == 18 )
+    {
+      $("#caribou").html("<img src=\"theme/caribou/img/caribou.png\" alt=\"Coucou le Caribou\" />");
+      $("#search-go").html("En avant Tabernacle !");
+      $("#search-input").attr("placeholder", "Entre ton caribou alsacien (ex: Märikhàll, Màtros) ");
+
+      state=0;
+    }
+  }, true);
+  }
+}());
 </script>';
 
 ?>
