129f43eea5466eef7c7c58b2081e7d0ae5650533
Karl Woditsch <k.woditsch@gmail.com>
2012-12-07 03:42:03 +0100

    Tag added

--- init.php
+++ init.php
@@ -11,6 +11,7 @@ use lib\tag\DateTag;
 spl_autoload_extensions('.php');
 spl_autoload_register();
 
+/*
 echo "TEST START<br/>";
 
 echo "DB START<br/>";
@@ -74,4 +75,32 @@ var_dump(Culture::getCultures());
 var_dump($c->getTags());
 
 echo "CULTURE END<br/>";
+*/
+
+/*
+$str = "Musée, Restaurant, Monument, Château, Piscine, Salle de sport, Bibliothèque, Zoo, Parc, Marché, Vignoble, Boutique";
+foreach(explode(", ", $str) as $word){
+	$t = new ThemeTag();
+	$t->setName($word);
+	Tag::saveTag($t);
+}
+$str = "Ain,Aisne,Allier,Alpes-de-Haute-Provence,Hautes-Alpes,Alpes-Maritimes,Ardèche,Ardennes,Ariège,Aube,Aude,Aveyron,Bouches-du-Rhône,Calvados,Cantal,Charente,Charente-Maritime,Cher,Corrèze,Corse-du-Sud,Haute-Corse,Côte-d-Or,Côtes-d-Armor,Creuse,Dordogne,Doubs,Drôme,Eure,Eure-et-Loir,Finistère,Gard,Haute-Garonne,Gers,Gironde,Hérault,Ille-et-Vilaine,Indre,Indre-et-Loire,Isère,Jura,Landes,Loir-et-Cher,Loire,Haute-Loire,Loire-Atlantique,Loiret,Lot,Lot-et-Garonne,Lozère,Maine-et-Loire,Manche,Marne,Haute-Marne,Mayenne,Meurthe-et-Moselle,Meuse,Morbihan,Moselle,Nièvre,Nord,Oise,Orne,Pas-de-Calais,Puy-de-Dôme ,Pyrénées-Atlantiques,Hautes-Pyrénées,Pyrénées-Orientales,Bas-Rhin,Haut-Rhin,Rhône,Haute-Saône,Saône-et-Loire,Sarthe,Savoie,Haute-Savoie,Paris,Seine-Maritime,Seine-et-Marne,Yvelines,Deux-Sèvres,Somme,Tarn,Tarn-et-Garonne,Var,Vaucluse,Vendée,Vienne,Haute-Vienne,Vosges,Yonne,Territoire-de-Belfort,Essonne,Hauts-de-Seine,Seine-Saint-Denis,Val-de-Marne,Val-d-Oise,Guadeloupe,Martinique,Guyane,La-Réunion,Mayotte";
+foreach(explode(",", $str) as $word){
+	$t = new PosTag();
+	$t->setName($word);
+	Tag::saveTag($t);
+}
+*/
+$str = "préhistoire,antiquité,moyen-age,moderne,contemporain,renaissance,révolution,lumières,industrielle,glorieuses,guerres";
+foreach(explode(",", $str) as $word){
+	$t = new PosTag();
+	$t->setName($word);
+	Tag::saveTag($t);
+}
+
+foreach(Culture::getCultures() as $c){
+	Culture::saveCulture($c);
+}
+
+
 ?>
